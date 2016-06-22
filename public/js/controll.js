aaa65151d035fa23ef3973deecd9f292f534081d
MiyajimaDaichi <let.it.be7daichi@gmail.com>
2016-06-22 22:42:45 +0900

    fix bugs

--- public/js/controll.js
+++ public/js/controll.js
@@ -2,58 +2,6 @@ myapp.controller('candyController', function ($scope, $firebaseArray,candyServic
     $("[class^=firsthide]").hide();
     $('.collapsible').collapsible();
     init(show);
-    //SearchPlace
-    $scope.searchPlace = function(text){
-        if($("#search_place").val() && $("#search_place").val().length > 0){
-            var request = {
-                location: googlemap.getCenter(),
-                radius: '500',
-                query: $("#search_place").val()
-            };
-            placeService = new google.maps.places.PlacesService(googlemap);
-            placeService.textSearch(request, function(results, status) {
-                if (status == google.maps.places.PlacesServiceStatus.OK) {
-                    for (var i = 0; i < results.length; i++) {
-                      var place = results[i];
-                    }
-                    $scope.places = results;
-                    $('.collapsible').collapsible();
-                }
-                
-            });
-        }
-    }
-    //Click More
-    $scope.placeclick = function(place){
-        var request = {
-            placeId: place.place_id
-        };
-        placeService.getDetails(request, function(placeDetail, status) {
-            $scope.rating ="error";
-            $scope.photos = null;
-            $scope.placeDetail = null;
-            if (status == google.maps.places.PlacesServiceStatus.OK) {
-                console.log(placeDetail);
-                if(placeDetail.rating){
-                    var rating = "rate rate" + Math.floor(placeDetail.rating) + "_" + Math.floor((placeDetail.rating - Math.floor(placeDetail.rating)) * 10);
-                    $scope.rating = rating;
-                }else{
-                    $scope.rating = "error"
-                }
-                $('#modalPlaceDetail').openModal();
-                var photos = new Array();
-                if(placeDetail.photos){
-                    angular.forEach(placeDetail.photos, function(value, key) {
-                        photos[key] = value.getUrl({'maxWidth': 300, 'maxHeight': 300});
-                    });
-                }
-                $scope.photos = photos;
-                $scope.placeDetail = placeDetail;
-                $scope.$apply();
-                $('.carousel').carousel();
-            }
-        });
-    }
     //Make pin from SearchPlace
     $scope.makeMeetUpMarker = function(place){
         if(Object.keys(markers_meet).length < 1){
@@ -105,7 +53,7 @@ myapp.controller('candyController', function ($scope, $firebaseArray,candyServic
     }
 });
 
-
+//Message Include
 myapp.controller('messageController', function ($scope, $firebaseArray,candyService) {
     var message = ref.child('sharemap').child(uniqueurl[2]).child('message').orderByChild("time");
     $scope.messages = $firebaseArray(message);
@@ -117,6 +65,62 @@ myapp.controller('messageController', function ($scope, $firebaseArray,candyServ
             $scope.messageInput = "";
             
         }
+    }
+});
+
+//Place Include
+myapp.controller('placeController', function ($scope, $firebaseArray,candyService) {
+    //SearchPlace
+    $scope.searchPlace = function(text){
+        if($("#search_place").val() && $("#search_place").val().length > 0){
+            var request = {
+                location: googlemap.getCenter(),
+                radius: '500',
+                query: $("#search_place").val()
+            };
+            placeService = new google.maps.places.PlacesService(googlemap);
+            placeService.textSearch(request, function(results, status) {
+                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                    for (var i = 0; i < results.length; i++) {
+                      var place = results[i];
+                    }
+                    $scope.places = results;
+                    $('.collapsible').collapsible();
+                }
+                
+            });
+        }
+    }
+    //Click More
+    $scope.placeclick = function(place){
+        var request = {
+            placeId: place.place_id
+        };
+        placeService.getDetails(request, function(placeDetail, status) {
+            $scope.rating ="error";
+            $scope.photos = null;
+            $scope.placeDetail = null;
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                console.log(placeDetail);
+                if(placeDetail.rating){
+                    var rating = "rate rate" + Math.floor(placeDetail.rating) + "_" + Math.floor((placeDetail.rating - Math.floor(placeDetail.rating)) * 10);
+                    $scope.rating = rating;
+                }else{
+                    $scope.rating = "error"
+                }
+                $('#modalPlaceDetail').openModal();
+                var photos = new Array();
+                if(placeDetail.photos){
+                    angular.forEach(placeDetail.photos, function(value, key) {
+                        photos[key] = value.getUrl({'maxWidth': 300, 'maxHeight': 300});
+                    });
+                }
+                $scope.photos = photos;
+                $scope.placeDetail = placeDetail;
+                $scope.$apply();
+                $('.carousel').carousel();
+            }
+        });
     }
 });
 
