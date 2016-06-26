3c13b0c09448310cc77075e4e56623760a3869f7
MiyajimaDaichi <let.it.be7daichi@gmail.com>
2016-06-26 12:44:28 +0900

    change directory

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     /* Google Map Screen*/
-    res.render(__dirname + '../views/initial/initial', { title: 'bad' });
+    res.render('../views/initial/initial', { title: 'bad' });
 });
 
 router.get('/sharemap/*', function(req, res, next) {
-    res.render(__dirname + '../views/index/index', { title: '' });
+    res.render('../views/index/index', { title: '' });
 });
 
 module.exports = router;
