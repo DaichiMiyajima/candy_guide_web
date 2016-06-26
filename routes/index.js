2f1eafe73c45128a4889708d7fbc24a281d8280e
MiyajimaDaichi <let.it.be7daichi@gmail.com>
2016-06-26 12:23:08 +0900

    change folder directory

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     /* Google Map Screen*/
-    res.render('initial', { title: 'bad' });
+    res.render(__dirname + '../views/initial/initial', { title: 'bad' });
 });
 
 router.get('/sharemap/*', function(req, res, next) {
-    console.log("sharemap");
     res.render('index', { title: '' });
 });
 
